80171be6fe5c46c6a6add7337238fb5919e7f449
rofrischmann <robin@rofrischmann.de>
2014-12-08 11:57:24 +0100

    splitted Objectfiles / Controlbutton

---
+++ ControlButton.js
@@ -0,0 +1,8 @@
+PerfectFont.ControlButton = function (perfectFont) {
+    this.init(perfectFont);
+}
+PerfectFont.ControlButton.prototype.init = function (window) {
+    this.window = window;
+}
+PerfectFont.ControlButton.prototype.show = function () {}
+PerfectFont.ControlButton.prototype.hide = function () {}
